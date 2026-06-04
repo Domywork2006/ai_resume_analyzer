dcd7ac1963dd731399263bae99a29c957bf374fa
Domywork2006 <itzvaishnav2006@gmail.com>
2026-06-04 10:56:01 +0530

    Checkpoint before resume analysis implementation

---
+++ lib/services/pdf_service.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+class PdfService {
+  Future<String> extractText(File file) async {
+    // Temporary placeholder
+    return "PDF selected: ${file.path}";
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,7 +9,6 @@ import cloud_firestore
 import file_picker
 import firebase_auth
 import firebase_core
-import firebase_storage
 import google_sign_in_ios
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -17,6 +16,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
 }
